de72824e99d32fae4726812116384eeed1c11ec2
Rozanna <54109278+RozannaBoczar@users.noreply.github.com>
2021-11-26 17:53:57 +0100

    added more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { ViewPostsComponent } from './view-posts/view-posts.component';
+import { SocialMediaPostsComponent } from './social-media-posts/social-media-posts.component';
+import { ManageGroupsComponent } from './manage-groups/manage-groups.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HelpComponent } from './help/help.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    UserPageComponent,
+    ViewPostsComponent,
+    SocialMediaPostsComponent,
+    ManageGroupsComponent,
+    SettingsComponent,
+    HelpComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/social-media-posts/social-media-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-media-posts',
+  templateUrl: './social-media-posts.component.html',
+  styleUrls: ['./social-media-posts.component.css']
+})
+export class SocialMediaPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
