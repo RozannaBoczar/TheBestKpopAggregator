91031ee8d47f5ffe7a16b6ca9f3572eadf38cfde
Rozanna <54109278+RozannaBoczar@users.noreply.github.com>
2021-12-30 12:05:15 +0100

    go

--- TheBestKpopAggregator/src/app/app-routing.module.ts
+++ TheBestKpopAggregator/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { UserPageComponent } from './user-page/user-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ViewPostsComponent } from './view-posts/view-posts.component';
 import { ChooseGroupComponent } from './choose-group/choose-group.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [ 
   { path: '', redirectTo: '/login-page', pathMatch: 'full' },
   { path: 'login-page', component: LoginPageComponent },
+  { path: 'register-page', component: RegisterPageComponent },
+  { path: 'user-page', redirectTo:'user-page/choose-groups' },
   { path: 'user-page', component: UserPageComponent,
   children: [
     //{path: 'help', component: HelpComponent},
